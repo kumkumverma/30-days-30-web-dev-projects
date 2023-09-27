947b18dd08ff56836462d653fccb1102cc9f3a8e
kumkumverma <81639466+kumkumverma@users.noreply.github.com>
2023-09-27 15:15:28 +0530

    Add API

---
+++ 03-image-search-engine-app/script.js
@@ -0,0 +1,21 @@
+const accessKey ="Zsl6FE9Sh9Eo5lvYapKa6q3bx8hevpK2Ve9JOq_FJ5k"
+
+const searchForm=document.getElementById('search-form');
+const searchBox=document.getElementById('search-box');
+const searchResult=document.getElementById('search-result');
+const searchMoreBtn=document.getElementById('show-more-btn');
+let keyword="";
+let page=1;
+async function searchImages(){
+    keyword=searchBox.value;
+    const url= `https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${accessKey}`;
+
+    const response =await fetch(url);
+    const data = await response.json();
+    console.log(data);
+}
+searchForm.addEventListener("submit",(e) =>{
+    e.preventDefault();
+    page=1;
+    searchImages();
+})
