f7f5882dffe013be1891aa49089dbbf4a5b089c7
kumkumverma <81639466+kumkumverma@users.noreply.github.com>
2023-09-30 22:48:31 +0530

    add function showquestion

--- 03-image-search-engine-app/script.js
+++ 03-image-search-engine-app/script.js
@@ -1,4 +1,4 @@
-const accessKey ="Zsl6FE9Sh9Eo5lvYapKa6q3bx8hevpK2Ve9JOq_FJ5k"
+const accessKey ="Zsl6FE9Sh9Eo5lvYapKa6q3bx8hevpK2Ve9JOq_FJ5k";
 
 const searchForm=document.getElementById('search-form');
 const searchBox=document.getElementById('search-box');
@@ -12,10 +12,22 @@ async function searchImages(){
 
     const response =await fetch(url);
     const data = await response.json();
-    console.log(data);
-}
+   
+    
+    const results =  data.results;
+    results.map((result) =>{
+        const image = document.createElement('img');
+        image.src = result.urls.small;
+        const imageLink = document.createElement("a");
+        imageLink.href = result.links.html;
+        imageLink.target ="_blank";
+         
+        imageLink.appendChild(image);
+        searchResult.appendChild(imageLink);
+    })
 searchForm.addEventListener("submit",(e) =>{
     e.preventDefault();
     page=1;
     searchImages();
-})
+}
+)};
