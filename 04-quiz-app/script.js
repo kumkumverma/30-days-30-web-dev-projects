caaa859349b7195dab8989d67db5f9629011be0b
kumkumverma <81639466+kumkumverma@users.noreply.github.com>
2023-10-02 14:15:49 +0530

    final commit

--- 04-quiz-app/script.js
+++ 04-quiz-app/script.js
@@ -14,7 +14,7 @@ const questions =[
             {text:"Vatican City", correct: true},
             {text:"Bhutan", correct: false},
             {text:"Nepal", correct: false},
-            {text:"Shree Lanka", correct: true},
+            {text:"Shree Lanka", correct: false},
         ]
     },
     {
@@ -38,7 +38,7 @@ const questions =[
 
 ]
 const questionElement=document.getElementById("question")
-const answerButton=document.getElementById("answer-buttons")
+const answerButtons=document.getElementById("answer-buttons")
 const nextButton=document.getElementById("next-btn")
 
 let currentQuestionIndex = 0;
@@ -50,16 +50,72 @@ function startQuiz(){
     showQuestion();
 }
 function showQuestion(){
+    resetState();
     let currentQuestion =   questions[currentQuestionIndex];
     let questionNo = currentQuestionIndex +1;
     questionElement.innerHTML =questionNo + ". " + currentQuestion.
     question;
     currentQuestion.answers.forEach(answer => {
-        const button=document.createElement("button");
+        const button=document.createElement("button"); 
         button.innerHTML = answer.text;
         button.classList.add("btn");
-        answerButton.appendChild(button);
+        answerButtons.appendChild(button);
+        if(answer.correct){
+            button.dataset.correct = answer.correct;
+        }
+        button.addEventListener("click", selectAnswer);
+
 
     }); 
 }
+function resetState(){
+    nextButton.style.display ="none";
+    while(answerButtons.firstChild){
+        answerButtons.removeChild(answerButtons.firstChild);
+    }
+}
+    function selectAnswer(e){
+        const selectedBtn = e.target;
+        const isCorrect= selectedBtn.dataset.correct ==="true";
+        if(isCorrect){
+            selectedBtn.classList.add("correct");
+            score++;
+        }else{
+            selectedBtn.classList.add("incorrect");
+        }
+        Array.from(answerButtons.children ).forEach(button =>{
+            if(button.dataset.correct ==="true"){
+                button.classList.add("correct");
+            }
+            button.disabled = true;
+        } );
+        nextButton.style.display ="block";
+        
+    }
+
+function showScore(){
+    resetState();
+    questionElement.innerHTML =`You Scored ${score} out of ${questions.lenth}!`;
+    nextButton.innerHTML= "Play Again";
+    nextButton.style.display="block"
+}
+
+function handleNextButton(){
+    currentQuestionIndex++;
+    if(currentQuestionIndex < questions.length){
+        showQuestion();
+    }else{
+        showScore();
+    }
+}
+
+    nextButton.addEventListener("click", ()=>{
+        if(currentQuestionIndex <questions.length ){
+            handleNextButton();
+        }else{
+            startQuiz();
+        }
+    })
+ 
+
 startQuiz();
